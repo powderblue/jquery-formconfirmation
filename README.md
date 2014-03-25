8901265927f3f118e808b8dc5aba7a7f698f3745
Dan Bettles <danbettles@yahoo.co.uk>
2014-03-25 15:20:14 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#jquery-formconfirmation#
+
+jquery-formconfirmation returns an object containing values of inputs listed against their labels.
---
+++ jquery.formconfirmation.js
@@ -0,0 +1,63 @@
+/*globals jQuery, window*/
+/**
+ * @author Dan Bettles <dan@powder-blue.com>
+ * @copyright Powder Blue Ltd 2014
+ */
+
+(function () {
+
+    /**
+     * Returns the label-text associated with the specified input.
+     * 
+     * @param {jQuery} oInputEl
+     * @returns {String|undefined}
+     */
+    function inputGetLabelText(oInputEl) {
+        var oLabelEl;
+
+        oLabelEl = oInputEl.closest('form').find('label[for="' + oInputEl.attr('id') + '"]');
+
+        if (oLabelEl.size() > 0) {
+            return oLabelEl.text();
+        }
+
+        oLabelEl = oInputEl.closest('label');
+
+        if (oLabelEl.size() > 0) {
+            return oLabelEl.children('span').text();
+        }
+
+        return undefined;
+    }
+
+    jQuery.fn.extend({
+
+        /**
+         * Returns an object containing values of inputs listed against their labels.
+         * 
+         * @return Object
+         */
+        formConfirmation: function () {
+            var oFirstEl = this.first(),
+                oLabelledValues = {};
+
+            oFirstEl.find(':input').each(function () {
+                var oInputEl = jQuery(this),
+                    labelText = inputGetLabelText(oInputEl),
+                    value = oInputEl.val();
+
+                if (labelText === undefined) {
+                    return true;
+                }
+
+                if (oInputEl.is('select')) {
+                    value = oInputEl.find('option[value="' + value + '"]').text();
+                }
+
+                oLabelledValues[labelText] = value;
+            });
+
+            return oLabelledValues;
+        }
+    });
+}());
